f31ba5d9c33b305612d5939dbb12bd7987474fc6
Justice <jb99087n@pace.edu>
2026-04-22 16:07:52 -0400

    Completed portfolio site

--- src/App.js
+++ src/App.js
@@ -1,4 +1,20 @@
-export default function Hello() {
-  return <p>Hello, world</p>;
+import "./App.css";
+import About from "./components/about";
+import Navbar from "./components/navbar";
+import Projects from "./components/projects";
+import Skills from "./components/skills";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <About />
+      <Skills />
+      <Projects />
+      <Contacts />
+    </div>
+  );
 }
+
+export default App;
 
