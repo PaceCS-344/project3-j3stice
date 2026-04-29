94e6a839da9ff0b752ceb81c5760604323f2ed15
Justice <jb99087n@pace.edu>
2026-04-29 16:39:18 -0400

    Completed ui interactive portfolio updates

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,14 +1,18 @@
+import ThemeToggle from "./ThemeToggle";
 
 function Navbar() {
     return (
       <nav className="navbar">
         <h2 className="logo">JB.</h2>
+
         <div className="nav-links">
           <a href="#about">About</a>
           <a href="#skills">Skills</a>
           <a href="#projects">Projects</a>
           <a href="#contact">Contact</a>
         </div>
+
+        <ThemeToggle />
       </nav>
     );
   }
